dea7cb0983958857bd038dd59f5f03459a3df5f5
JATIN KUMAR <jatinnith@gmail.com>
2018-07-19 17:05:12 +0000

    Create email.php

---
+++ email.php
@@ -0,0 +1,42 @@
+<?php
+
+    $errorMessage="";
+    $successMessage="";
+    if($_POST)
+    {
+        if($_POST["remail"]=="")
+        {
+            $errorMessage=$errorMessage."Reciever's email is missing<br>";
+        }
+        if($_POST["semail"]=="")
+        {
+            $errorMessage.="sender email is missing<br>";
+        }
+        if($_POST["subject"]=="")
+        {
+            $errorMessage.="subject is missing<br>";
+        }
+        if($_POST["message"]=="")
+        {
+            $errorMessage.="message is missing<br>";
+        }
+        if($errorMessage!="")
+        {
+            $errorMessage='<div class="alert alert-danger" role="alert"><b>There are error(s) in your form:<br></b>'.$errorMessage.'</div>';
+        }
+        else
+        {   
+            $header="From:".$_POST["semail"];
+            if(mail($_POST["remail"],$_POST["subject"],$_POST["message"],$header))
+            {
+                $successMessage='<div class="alert alert-success" role="alert">Your message has been sent!</div>';
+            }
+            else
+            {
+                $errorMessage='<div class="alert alert-danger" role="alert"><b>Email not sent<br></b></div>';
+            }
+        }
+    }
+
+
+?>
